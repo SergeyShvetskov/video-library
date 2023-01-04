d1e0a6598ce9bf7f80a3442c485005dcd0c6ccd4
Prysmytskyi-Oleksandr <san2003@ukr.net>
2023-01-04 10:21:34 +0200

    add_fetch_trend

--- src/js/fetchMove.js
+++ src/js/fetchMove.js
@@ -1,35 +1,46 @@
 const axios = require('axios').default;
-export default class NewsApiService {
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
 
-  async fetchMoves() {
-    const URL = 'https://api.themoviedb.org/3/search/movie';
-    const KEY = 'a79202f0028fac6a27982a88fb1459a6';
+export default class MovesApiService {
+        API_KEY = 'a79202f0028fac6a27982a88fb1459a6';
+        COMMON_URL = 'https://api.themoviedb.org/3/';
+        TRENDING_FilM = 'trending/all/day';
+        SEARCH_FilM = 'search/movie';
+        
+        constructor() {
+                this.searchQuery = '';
+                this.page = 1;
+        }
 
-    const responseAxios = await axios.get(
-      `${URL}?api_key=${KEY}&query=${this.searchQuery}&page=${this.page}`
-    );
-    try {
-      console.log(responseAxios);
-      const response = responseAxios.data;
-      this.page += 1;
-      return response;
-    } catch (error) {
-      console.error(error);
-    }
-  }
+        async fetchTrendMoves() {
+                                              
+                const responseAxios = await axios.get(`${this.COMMON_URL}${this.TRENDING_FilM}?api_key=${this.API_KEY}&page=${this.page}`);
+                try {
+                        console.log(responseAxios);
+                        const response = responseAxios.data;
+                        this.page += 1;
+                        return response;
+                }   catch (error) {
+    console.error(error);
+  }     
+                                };   
 
-  get query() {
-    return this.searchQuery;
-  }
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
+        
+        async fetchSearchMoves() {
+                                             
+                const responseAxios = await axios.get(`${this.COMMON_URL}${this.SEARCH_FilM}?api_key=${this.API_KEY}&query=${this.searchQuery}&page=${this.page}`);
+                try {
+                        console.log(responseAxios);
+                        const response = responseAxios.data;
+                        this.page += 1;
+                        return response;
+                }   catch (error) {
+    console.error(error);
+  }     
+                                };                         
+        
+        get query() { return this.searchQuery };
+        set query(newQuery) { this.searchQuery = newQuery };
 
-  resetPage() {
-    this.page = 1;
-  }
+        resetPage() {this.page = 1 };    
+
 }
--- src/js/filmCards-home.js
+++ src/js/filmCards-home.js
@@ -1,14 +1,23 @@
 import allGenres from './genres.json';
+import MovesApiService from './fetchMove';
 
-const cardsListLibrary = document.querySelector('.cards__list--library');
-const cardsList = document.querySelector('.cards__list');
+const refs = {
+  cardsListLibrary: document.querySelector('.cards__list--library'),
+  cardsList: document.querySelector('.cards__list'),
+  }
 
-//создание карточки
-function createCard(data) {
-  return data
-    .map(obj => {
-      const { id, poster_path, title, release_date, genre_ids } = obj;
-      return `<li class="cards__item" id="${id}">
+const movieGalleryFetch = new MovesApiService();
+
+movieGalleryFetch
+    .fetchTrendMoves()
+  .then((response) => {createCard(response);
+  
+  }).catch(err => err.message);
+
+
+function createCard(response) {
+  const card = response.results.map(({ id, poster_path, title, release_date, genre_ids }) => 
+           `<li class="cards__item" id="${id}">
         <a class="cards__link">
             <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
         </a>
@@ -19,15 +28,12 @@ function createCard(data) {
               genre_ids
             )} | ${createYear(release_date)}</p>
             </div>
-        </li>`;
-    })
-    .join('');
+        </li>`
+    )
+        .join('');
+    refs.cardsList.insertAdjacentHTML("beforeend", card);
 }
 
-//вставка разметки
-function insertMarkup(htmlMarkup, htmlEl) {
-  htmlEl.innerHTML = htmlMarkup;
-}
 //отображение года выпуска
 function createYear(data) {
   if (data) {
@@ -36,6 +42,7 @@ function createYear(data) {
     return (data = 'Not found');
   }
 }
+
 //обрезка название
 function getShortName(string) {
   if (string) {
@@ -45,6 +52,7 @@ function getShortName(string) {
     return string;
   }
 }
+
 // жанр
 const { genres } = allGenres;
 function findGenresOfMovie(ids) {
@@ -53,6 +61,7 @@ function findGenresOfMovie(ids) {
   if (movieGenres.length > 2) {
     const removedGenres = movieGenres.splice(0, 2);
     removedGenres.push('Other');
+
     return removedGenres.join(', ');
   }
   if (movieGenres.length === 0) {
@@ -60,6 +69,7 @@ function findGenresOfMovie(ids) {
   }
   return movieGenres.join(', ');
 }
+
 export {
   createCard,
   insertMarkup,
@@ -67,3 +77,99 @@ export {
   cardsList,
   cardsListLibrary,
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import allGenres from './genres.json';
+
+// const cardsListLibrary = document.querySelector('.cards__list--library');
+// const cardsList = document.querySelector('.cards__list');
+
+// //создание карточки
+// function createCard(data) {
+//   return data
+//     .map(obj => {
+//       const { id, poster_path, title, release_date, genre_ids } = obj;
+//       return `<li class="cards__item" id="${id}">
+//         <a class="cards__link">
+//             <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
+//         </a>
+//             <div class="cards__text"><h2 class="cards__name">${getShortName(
+//               title
+//             )}</h2>
+//             <p class="cards__genres"> ${findGenresOfMovie(
+//               genre_ids
+//             )} | ${createYear(release_date)}</p>
+//             </div>
+//         </li>`;
+//     })
+//     .join('');
+// }
+
+// //вставка разметки
+// function insertMarkup(htmlMarkup, htmlEl) {
+//   htmlEl.innerHTML = htmlMarkup;
+// }
+// //отображение года выпуска
+// function createYear(data) {
+//   if (data) {
+//     return data.slice(0, 4);
+//   } else {
+//     return (data = 'Not found');
+//   }
+// }
+// //обрезка название
+// function getShortName(string) {
+//   if (string) {
+//     if (string.length >= 32) {
+//       return string.substr(0, 32) + '...';
+//     }
+//     return string;
+//   }
+// }
+// // жанр
+// const { genres } = allGenres;
+// function findGenresOfMovie(ids) {
+//   const arr = ids.flatMap(id => genres.filter(element => element.id === id));
+//   let movieGenres = arr.map(el => el.name);
+//   if (movieGenres.length > 2) {
+//     const removedGenres = movieGenres.splice(0, 2);
+//     removedGenres.push('Other');
+//     return removedGenres.join(', ');
+//   }
+//   if (movieGenres.length === 0) {
+//     return (movieGenres = 'Not found');
+//   }
+//   return movieGenres.join(', ');
+// }
+// export {
+//   createCard,
+//   insertMarkup,
+//   findGenresOfMovie,
+//   cardsList,
+//   cardsListLibrary,
+// };
