3716721db5380a01cfde4229c3319a13d5ee7f52
MarynaAponasko <aponasko.mari@gmail.com>
2023-01-04 23:36:48 +0200

    create gallery files

---
+++ src/js/filmCards-library.js
@@ -0,0 +1,25 @@
+// const refs = {
+//   watchedBtnLb: document.querySelector('.watchedBtnLb'),
+//   queueBtnLb: document.querySelector('.queueBtnLb'),
+// };
+
+// refs.watchedBtnLb.addEventListener('click', onWatchedBtnLbClick);
+
+// function onWatchedBtnLbClick(e) {
+//   console.log('click lib');
+//   createWathedGalery();
+//   //   createWatchedList();
+// }
+
+// function createWatchedList() {
+//   for (let i = 0; i < localStorage.length; i += 1) {
+//     console.log(localStorage.length);
+//   }
+// }
+//   for (let i = 0; i < localStorage.length; i++) {
+//     console.log(localStorage.length);
+//     if (localStorage.key(i).includes('watched')) {
+//       console.log(localStorage.getItem(localStorage.key(i)));
+//     }
+//   }
+// }
