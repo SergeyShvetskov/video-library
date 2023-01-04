2ef3388939e073a5eb832fa0f7833809ed03bef7
MarynaAponasko <aponasko.mari@gmail.com>
2023-01-04 23:31:30 +0200

    remove id from localStorage

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,26 +1,27 @@
-
-const modalCloseBtn = document.querySelector("[data-modal-close]");
-const modal = document.querySelector("[data-modal]");
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modal = document.querySelector('[data-modal]');
 
 modalCloseBtn.addEventListener('click', toggleModal);
 document.addEventListener('keydown', closeModalEsc);
 document.addEventListener('click', closeModalClick);
 
-
 function toggleModal() {
-    modal.classList.add("is-hidden");
+  modal.classList.add('is-hidden');
+  localStorage.removeItem('id-movie');
 }
 
 function closeModalClick(e) {
-    if (e.target === modal) {
-        modal.classList.add("is-hidden");
-    }
+  if (e.target === modal) {
+    modal.classList.add('is-hidden');
+    localStorage.removeItem('id-movie');
+  }
 }
-  
+
 function closeModalEsc(e) {
-    if (e.keyCode === 27) {
-        modal.classList.add("is-hidden");
-    }
+  if (e.keyCode === 27) {
+    modal.classList.add('is-hidden');
+    localStorage.removeItem('id-movie');
+  }
 }
 
 export { modal };
