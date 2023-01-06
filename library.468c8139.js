2f61f5851f90b3374230800408543e068c95e10b
SergeyShvetskov <svetsoff@gmail.com>
2023-01-06 15:42:12 +0000

    Deploying to gh-pages from @ SergeyShvetskov/video-library@8e9b84765b5697327cdb96434921867252295927 🚀

--- library.468c8139.js
+++
@@ -1,2 +0,0 @@
-!function(){const o=document.querySelector(".to__top");window.onscroll=()=>{window.scrollY>100?o.classList.add("to__top--show"):window.scrollY<100&&o.classList.remove("to__top--show")},o.onclick=()=>{window.scrollTo(0,0)}}();
-//# sourceMappingURL=library.468c8139.js.map
