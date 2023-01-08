fb257fff36fd0a536e574c39251f5a60599512d4
User61 <svetsoff@gmail.com>
2023-01-08 20:43:04 +0200

    upd

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -29,21 +29,21 @@ const options = {
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    page: '<div href="#" class="tui-page-btn">{{page}}</div>',
     currentPage:
       '<strong class="tui-page-btn tui-is-selected" id="current">{{page}}</strong>',
     moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<div href="#" class="tui-page-btn tui-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
+      '</div>',
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
       '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<div href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
       '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
+      '</div>',
   },
 };
 
