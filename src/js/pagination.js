85ba47776328e82187df84b08e9cc3ac2046b370
User61 <svetsoff@gmail.com>
2023-01-08 18:19:55 +0200

    upd

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,8 +1,11 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
-import MovesApiService from './fetchMove';
+// import MovesApiService from './fetchMove';
 import allGenres from './genres.json';
 import { Notify } from 'notiflix';
+import { fetchSearchMove, fetchTrendMoves } from './fetch';
+import { createCard } from './func-create-cadr';
+import { inputRef } from './refs';
 
 const refs = {
   cardsListLibrary: document.querySelector('.cards__list--library'),
@@ -15,14 +18,14 @@ const container = document.getElementById('pagination');
 
 const itemsPerPage = 20;
 
-const moves = new MovesApiService();
+// const moves = new MovesApiService();
 
 const options = {
   totalItems: 1000,
   itemsPerPage: itemsPerPage,
   visiblePages: 5,
   page: 1,
-  centerAlign: false,
+  centerAlign: true,
   firstItemClassName: 'tui-first-child',
   lastItemClassName: 'tui-last-child',
   template: {
@@ -46,227 +49,249 @@ const options = {
 
 const pagination = new Pagination(container, options);
 
-const page = pagination.getCurrentPage();
+// const page = pagination.getCurrentPage();
 
-function getShortName(string) {
-  if (string) {
-    if (string.length >= 32) {
-      return string.substr(0, 32) + '...';
-    }
-    return string;
-  }
-}
-const { genres } = allGenres;
-function findGenresOfMovie(ids) {
-  const arr = ids.flatMap(id => genres.filter(element => element.id === id));
-  let movieGenres = arr.map(el => el.name);
-  if (movieGenres.length > 2) {
-    const removedGenres = movieGenres.splice(0, 2);
-    removedGenres.push('Other');
+// function getShortName(string) {
+//   if (string) {
+//     if (string.length >= 32) {
+//       return string.substr(0, 32) + '...';
+//     }
+//     return string;
+//   }
+// }
+// const { genres } = allGenres;
+// function findGenresOfMovie(ids) {
+//   const arr = ids.flatMap(id => genres.filter(element => element.id === id));
+//   let movieGenres = arr.map(el => el.name);
+//   if (movieGenres.length > 2) {
+//     const removedGenres = movieGenres.splice(0, 2);
+//     removedGenres.push('Other');
 
-    return removedGenres.join(', ');
-  }
-  if (movieGenres.length === 0) {
-    return (movieGenres = 'Not found');
-  }
-  return movieGenres.join(', ');
-}
-function createYear(data) {
-  if (data) {
-    return data.slice(0, 4);
-  } else {
-    return (data = 'Not found');
-  }
-}
+//     return removedGenres.join(', ');
+//   }
+//   if (movieGenres.length === 0) {
+//     return (movieGenres = 'Not found');
+//   }
+//   return movieGenres.join(', ');
+// }
+// function createYear(data) {
+//   if (data) {
+//     return data.slice(0, 4);
+//   } else {
+//     return (data = 'Not found');
+//   }
+// }
 
+// function loadMoreTrendMoves(event) {
+//   const currentPage = event.page;
+//   console.log(currentPage);
+
+//   moves
+//     .fetchTrendMoves(currentPage)
+//     .then(response => {
+//       response.page = currentPage;
+
+//       const markup = response.results
+//         .map(
+//           ({
+//             id,
+//             poster_path,
+//             title,
+//             release_date,
+//             genre_ids,
+//           }) => `<li class="cards__item" id="${id}">
+//         <a class="cards__link">
+//             <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
+//         </a>
+//             <div class="cards__text"><h2 class="cards__name">${getShortName(
+//               title
+//             )}</h2>
+//             <p class="cards__genres"> ${findGenresOfMovie(
+//               genre_ids
+//             )} | ${createYear(release_date)}</p>
+//             </div>
+//         </li>`
+//         )
+//         .join('');
+//       refs.cardsList.innerHTML = markup;
+//     })
+//     .catch(error => {
+//       Notify.failure(error.message);
+//       refs.pagination.classList.add('is-hidden');
+//     });
+// }
 function loadMoreTrendMoves(event) {
-  const currentPage = event.page;
-  console.log(currentPage);
+  // console.log(event.page);
 
-  moves
-    .fetchTrendMoves(currentPage)
-    .then(response => {
-      response.page = currentPage;
+  if (inputRef.value !== '') {
+    fetchSearchMove(inputRef.value, event.page)
+      .then(response => {
+        console.log(`response.total_results:${response.total_results}`);
 
-      const markup = response.results
-        .map(
-          ({
-            id,
-            poster_path,
-            title,
-            release_date,
-            genre_ids,
-          }) => `<li class="cards__item" id="${id}">
-        <a class="cards__link">
-            <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
-        </a>
-            <div class="cards__text"><h2 class="cards__name">${getShortName(
-              title
-            )}</h2>
-            <p class="cards__genres"> ${findGenresOfMovie(
-              genre_ids
-            )} | ${createYear(release_date)}</p>
-            </div>
-        </li>`
-        )
-        .join('');
-      refs.cardsList.innerHTML = markup;
-    })
-    .catch(error => {
-      Notify.failure(error.message);
-      refs.pagination.classList.add('is-hidden');
-    });
+        refs.cardsList.innerHTML = '';
+        createCard(response);
+      })
+      .catch(err => Notiflix.Notify.failure(err));
+  } else {
+    fetchTrendMoves(event.page)
+      .then(response => {
+        // console.log(`response.total_results:${response.total_results}`);
+        options.totalItems = response.total_results;
+        refs.cardsList.innerHTML = '';
+        createCard(response);
+      })
+      .catch(err => Notiflix.Notify.failure(err));
+  }
 }
-
 pagination.on('beforeMove', loadMoreTrendMoves);
 
-moves
-  .fetchTrendMoves(page)
-  .then(({ results, total }) => {
-    if (results.length === 0) {
-      Notify.warning(`We don\`t find any moves 🤷‍♀️`);
-      return;
-    }
+// moves
+//   .fetchTrendMoves(page)
+//   .then(({ results, total }) => {
+//     if (results.length === 0) {
+//       Notify.warning(`We don\`t find any moves 🤷‍♀️`);
+//       return;
+//     }
 
-    pagination.reset(total);
-    const markup = results
-      .map(
-        ({
-          id,
-          poster_path,
-          title,
-          release_date,
-          genre_ids,
-        }) => `<li class="cards__item" id="${id}">
-        <a class="cards__link">
-            <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
-        </a>
-            <div class="cards__text"><h2 class="cards__name">${getShortName(
-              title
-            )}</h2>
-            <p class="cards__genres"> ${findGenresOfMovie(
-              genre_ids
-            )} | ${createYear(release_date)}</p>
-            </div>
-        </li>`
-      )
-      .join('');
+//     pagination.reset(total);
+//     const markup = results
+//       .map(
+//         ({
+//           id,
+//           poster_path,
+//           title,
+//           release_date,
+//           genre_ids,
+//         }) => `<li class="cards__item" id="${id}">
+//         <a class="cards__link">
+//             <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
+//         </a>
+//             <div class="cards__text"><h2 class="cards__name">${getShortName(
+//               title
+//             )}</h2>
+//             <p class="cards__genres"> ${findGenresOfMovie(
+//               genre_ids
+//             )} | ${createYear(release_date)}</p>
+//             </div>
+//         </li>`
+//       )
+//       .join('');
 
-    refs.cardsList.insertAdjacentHTML('beforeend', markup);
+//     refs.cardsList.insertAdjacentHTML('beforeend', markup);
 
-    refs.pagination.classList.remove('is-hidden');
-  })
-  .catch(error => {
-    Notify.failure(error.message);
-  });
+//     refs.pagination.classList.remove('is-hidden');
+//   })
+//   .catch(error => {
+//     Notify.failure(error.message);
+//   });
 
-const loadMoreMovesByQuery = event => {
-  const currentPage = event.page;
-  moves
-    .fetchSearchMoves(currentPage)
-    .then(({ results }) => {
-      const markup = results
-        .map(
-          ({
-            id,
-            poster_path,
-            title,
-            release_date,
-            genre_ids,
-          }) => `<li class="cards__item" id="${id}">
-        <a class="cards__link">
-            <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
-        </a>
-            <div class="cards__text"><h2 class="cards__name">${getShortName(
-              title
-            )}</h2>
-            <p class="cards__genres"> ${findGenresOfMovie(
-              genre_ids
-            )} | ${createYear(release_date)}</p>
-            </div>
-        </li>`
-        )
-        .join('');
+// const loadMoreMovesByQuery = event => {
+//   const currentPage = event.page;
+//   moves
+//     .fetchSearchMoves(currentPage)
+//     .then(({ results }) => {
+//       const markup = results
+//         .map(
+//           ({
+//             id,
+//             poster_path,
+//             title,
+//             release_date,
+//             genre_ids,
+//           }) => `<li class="cards__item" id="${id}">
+//         <a class="cards__link">
+//             <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
+//         </a>
+//             <div class="cards__text"><h2 class="cards__name">${getShortName(
+//               title
+//             )}</h2>
+//             <p class="cards__genres"> ${findGenresOfMovie(
+//               genre_ids
+//             )} | ${createYear(release_date)}</p>
+//             </div>
+//         </li>`
+//         )
+//         .join('');
 
-      refs.cardsList.innerHTML = markup;
-    })
-    .catch(error => {
-      Notify.failure(error.message);
-    });
-};
+//       refs.cardsList.innerHTML = markup;
+//     })
+//     .catch(error => {
+//       Notify.failure(error.message);
+//     });
+// };
 
-const handleSubmit = event => {
-  event.preventDefault();
+// const handleSubmit = event => {
+//   event.preventDefault();
 
-  const {
-    elements: { query },
-  } = event.target;
-  const searchQuery = query.value.trim();
-  // console.log(searchQuery);
+//   const {
+//     elements: { query },
+//   } = event.target;
+//   const searchQuery = query.value.trim();
+//   // console.log(searchQuery);
 
-  if (!searchQuery) {
-    Notify.failure('Input query!!!');
-    return;
-  }
+//   if (!searchQuery) {
+//     Notify.failure('Input query!!!');
+//     return;
+//   }
 
-  if (moves.query === searchQuery) {
-    Notify.info('Already show!');
-    return;
-  }
+//   if (moves.query === searchQuery) {
+//     Notify.info('Already show!');
+//     return;
+//   }
 
-  moves.query = searchQuery;
-  // console.log(moves.query);
+//   moves.query = searchQuery;
+//   // console.log(moves.query);
 
-  refs.cardsList.innerHTML = '';
+//   refs.cardsList.innerHTML = '';
 
-  moves
-    .fetchSearchMoves(page)
-    .then(({ results, total }) => {
-      if (results.length === 0) {
-        Notify.warning(`We don\`t find any moves by ${searchQuery} 🤷‍♀️`);
-        refs.pagination.classList.add('is-hidden');
-        return;
-      }
+//   moves
+//     .fetchSearchMoves(page)
+//     .then(({ results, total }) => {
+//       if (results.length === 0) {
+//         Notify.warning(`We don\`t find any moves by ${searchQuery} 🤷‍♀️`);
+//         refs.pagination.classList.add('is-hidden');
+//         return;
+//       }
 
-      pagination.off('beforeMove', loadMoreTrendMoves);
-      pagination.off('beforeMove', loadMoreMovesByQuery);
-      pagination.on('beforeMove', loadMoreMovesByQuery);
+//       pagination.off('beforeMove', loadMoreTrendMoves);
+//       pagination.off('beforeMove', loadMoreMovesByQuery);
+//       pagination.on('beforeMove', loadMoreMovesByQuery);
 
-      pagination.reset(total);
+//       pagination.reset(total);
 
-      const markup = results
-        .map(
-          ({
-            id,
-            poster_path,
-            title,
-            release_date,
-            genre_ids,
-          }) => `<li class="cards__item" id="${id}">
-        <a class="cards__link">
-            <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
-        </a>
-            <div class="cards__text"><h2 class="cards__name">${getShortName(
-              title
-            )}</h2>
-            <p class="cards__genres"> ${findGenresOfMovie(
-              genre_ids
-            )} | ${createYear(release_date)}</p>
-            </div>
-        </li>`
-        )
-        .join('');
+//       const markup = results
+//         .map(
+//           ({
+//             id,
+//             poster_path,
+//             title,
+//             release_date,
+//             genre_ids,
+//           }) => `<li class="cards__item" id="${id}">
+//         <a class="cards__link">
+//             <img class="cards__img" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}" loading="lazy">
+//         </a>
+//             <div class="cards__text"><h2 class="cards__name">${getShortName(
+//               title
+//             )}</h2>
+//             <p class="cards__genres"> ${findGenresOfMovie(
+//               genre_ids
+//             )} | ${createYear(release_date)}</p>
+//             </div>
+//         </li>`
+//         )
+//         .join('');
 
-      refs.cardsList.insertAdjacentHTML('beforeend', markup);
+//       refs.cardsList.insertAdjacentHTML('beforeend', markup);
 
-      refs.pagination.classList.remove('is-hidden');
-    })
-    .catch(error => {
-      Notify.failure(error.message);
-      refs.pagination.classList.add('is-hidden');
-    });
+//       refs.pagination.classList.remove('is-hidden');
+//     })
+//     .catch(error => {
+//       Notify.failure(error.message);
+//       refs.pagination.classList.add('is-hidden');
+//     });
 
-  event.target.reset();
-};
+//   event.target.reset();
+// };
 
-refs.searchForm.addEventListener('submit', handleSubmit);
+// refs.searchForm.addEventListener('submit', handleSubmit);
