1d28b39979d8eabca29fb65970e104cc3a45cf24
User61 <svetsoff@gmail.com>
2023-01-09 17:41:26 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,4 @@
 import Notiflix from 'notiflix';
-// import MovesApiService from './fetchMove';
 import { createCard } from './func-create-cadr';
 import { fetchSearchMove, fetchTrendMoves } from './fetch';
 import { currentPage } from './pagination-new';
@@ -7,7 +6,6 @@ import { currentPage } from './pagination-new';
 const axios = require('axios').default;
 
 document.addEventListener('DOMContentLoaded', startPage);
-// const currentPag = document.querySelector('pagination--current');
 
 const refs = {
   searchForm: document.querySelector('.header-search__wrapper'),
@@ -58,47 +56,6 @@ async function onSearch(event) {
   }
 }
 
-// const movieGalleryFetch = new MovesApiService();
-
-// movieGalleryFetch
-//   .fetchTrendMoves()
-//   .then(response => {
-//     createCard(response);
-//   })
-//   .catch(err => err.message);
-
-// refs.searchForm.addEventListener('submit', onSearch);
-
-// async function onSearch(event) {
-//   event.preventDefault();
-//   inputRef = event.currentTarget.elements.query.value.trim();
-//   movieGalleryFetch.searchQuery =
-//     event.currentTarget.elements.query.value.trim();
-//   movieGalleryFetch.resetPage();
-//   console.log(movieGalleryFetch.searchQuery);
-
-//   clear();
-
-//   if (movieGalleryFetch.searchQuery === '') {
-//     return;
-//   }
-
-//   await movieGalleryFetch
-//     .fetchSearchMoves()
-//     .then(response => {
-//       if (response.results.length === 0) {
-//         console.lof('помилка');
-//         return Notiflix.Notify.failure(
-//           `Search result not successful. Enter the correct movie name and try again.`
-//         );
-//       } else {
-//         createCard(response);
-//       }
-//     })
-//     .catch(err => err.message);
-// }
-
-//
 function clear() {
   refs.cardlist.innerHTML = '';
 }
